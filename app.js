fcbf40d48d19f9efe800c37f126dfee0477294ee
Sean Wines <seanwinesdev@gmail.com>
2017-03-08 18:13:45 -0700

    Created header and slideout nav

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 
+$(function() {
+
+  $(window).on('scroll', function () {
+    // Slide logo o left
+    $('.logo').css({
+      'left': '2rem',
+      'transform': 'translateX(0)'
+    });
+  })
+
+  // Show Nav
+  $('.fa-bars').on('click', function () {
+    $('.main-nav').toggleClass('show-nav');
+    $(this).toggleClass('fa-bars fa-close');
+
+  })
+
+})
